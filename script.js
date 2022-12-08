571429f80433502825963a3c8aae80013d4dd228
gains7 <102962254+gains7@users.noreply.github.com>
2022-12-08 18:20:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center:  [-40.2820546, -20.3318583 ],
+        zoom: 18,
+      })
