5c7f6ba21c13e8fa01f19febaf67f43a03484cf6
gains7 <102962254+gains7@users.noreply.github.com>
2022-12-08 18:20:13 -0300

    Delete script (2).js

--- script (2).js
+++
@@ -1,6 +0,0 @@
-var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center:  [-40.2820546, -20.3318583 ],
-        zoom: 18,
-      })
